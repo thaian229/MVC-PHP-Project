19caeeec45414f88d4dca52a9afe89056a5c8b58
Nguyễn Thái An <thaian229@gmail.com>
2021-05-19 14:33:27 +0700

    Update Framwork

--- public/index.php
+++ public/index.php
@@ -3,11 +3,8 @@
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(dirname(__FILE__)));
 
-if (isset($_GET['url']))
-{
+if (isset($_GET['url'])) {
     $url = $_GET['url'];
 }
 
 require_once (ROOT . DS . 'library' . DS . 'bootstrap.php');
-
-?>
