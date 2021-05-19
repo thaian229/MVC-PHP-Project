62c53539b0a894b193a2958126be86efb98d4ef7
Bùi Tú Hoàng <buituhoang@gmail.com>
2021-05-20 00:03:53 +0700

    Update config.php

--- config.php
+++ config.php
@@ -3,9 +3,9 @@
 /** Configuration Variables **/
 
 define('DB_NAME', 'phim_online');
-define('DB_USER', 'mhoang');
-define('DB_PASSWORD', '04126152');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
 
-define('BASE_PATH', 'http://localhost/PHP/WebProgramming/FinalProject/');
+define('BASE_PATH', 'http://localhost/NewPhpProject/MVC-PHP-Project');
 
