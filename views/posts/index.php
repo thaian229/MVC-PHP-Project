bb47c346e860e226f358e80ccafcb522ca602521
Bùi Tú Hoàng <buituhoang@gmail.com>
2021-05-21 21:36:12 +0700

    Update index.php

--- views/posts/index.php
+++ views/posts/index.php
@@ -1,41 +1,16 @@
 <?php
-//echo '<table align="center">';
-//
-//echo '<tr>';
-//foreach ($posts as $post) {
-//    if($post) {
-//        echo'
-//        <td style="margin-left: 2vw; margin-right: 2vw; width: 20vw; height: 30vh; border: 1px solid black" align="center">
-//            <a href="index.php?controller=posts&action=showPost&id=' . $post->id . '" style="text-decoration: none;">
-//                <div>
-//                    <img src="' . $post->thumbnailUrl . '" width="200" height="100">
-//                    <br/>
-//                    <p align="left"><strong style="color: #222222">' . $post->title . '</strong></p>
-//                </div>
-//            </a>
-//        </td>';
-//    }
-//}
-//echo '</tr>';
-//
-//echo '</table>';
-
-
-
-
-
 echo '<table align="center">';
 
 echo '<tr>';
 for($i = 0; $i < 4; $i++) {
-    if($posts[$i]) {
+    if(!empty($posts[$i])) {
         echo'
-        <td style="
+        <td class="thumbnail_item" style="
                 border: 1px solid black;
             " 
             align="center">
             <a href="index.php?controller=posts&action=showPost&id=' . $posts[$i]->id . '" style="text-decoration: none;">
-                <div>
+                <div class="thumbnail">
                     <img src="' . $posts[$i]->thumbnailUrl . '" width="240" height="180">
                 </div>
             </a>
@@ -45,15 +20,49 @@ for($i = 0; $i < 4; $i++) {
 echo '</tr>';
 echo '<tr>';
 for($i = 0; $i < 4; $i++) {
-    if($posts[$i]) {
+    if(!empty($posts[$i])) {
         echo'
-        <td style="
-            margin-right: 200px;
+        <td class="title_item" style="
             width: 15vw;
-            height: 10vh;
             border: 1px solid black">
             <a href="index.php?controller=posts&action=showPost&id=' . $posts[$i]->id . '" style="text-decoration: none;">
-                <div>
+                <div class="title">
+                    <p align="left"><strong style="color: #222222">' . $posts[$i]->title . '</strong></p>
+                </div>
+            </a>
+        </td>';
+    }
+}
+echo '</table>';
+echo '<br/><br/>';
+echo '<table align="center">';
+echo '<tr>';
+for($i = 4; $i < 8; $i++) {
+    if(!empty($posts[$i])) {
+        echo'
+        <td class="thumbnail_item" style="
+                border: 1px solid black;
+            " 
+            align="center">
+            <a href="index.php?controller=posts&action=showPost&id=' . $posts[$i]->id . '" style="text-decoration: none;">
+                <div class="thumbnail">
+                    <img src="' . $posts[$i]->thumbnailUrl . '" width="240" height="180">
+                </div>
+            </a>
+        </td>';
+    }
+    else continue;
+}
+echo '</tr>';
+echo '<tr>';
+for($i = 4; $i < 8; $i++) {
+    if(!empty($posts[$i])) {
+        echo'
+        <td class="title_item" style="
+            width: 15vw;
+            border: 1px solid black">
+            <a href="index.php?controller=posts&action=showPost&id=' . $posts[$i]->id . '" style="text-decoration: none;">
+                <div class="title">
                     <p align="left"><strong style="color: #222222">' . $posts[$i]->title . '</strong></p>
                 </div>
             </a>
