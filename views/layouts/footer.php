3ca062552cc617c5ac4ea8a2cfe61dbdb20eb2f0
Bùi Tú Hoàng <buituhoang@gmail.com>
2021-05-26 16:57:40 +0700

    Create header and footer

---
+++ views/layouts/footer.php
@@ -0,0 +1,45 @@
+<footer>
+    <div class="row_footer">
+        <div class="column">
+            <h2>About Us</h2>
+            <p>Three guys from HUST</p>
+            <p>Nothing special</p>
+            <br/>
+            <p>All clips are collected from outside sources.</p>
+            <p>No videos are hosted on this server.</p>
+        </div>
+        <div class="column">
+            <h2>Contact</h2>
+            <table style="color: white">
+                <tr>
+                    <td>
+                        <div class="crl-icon">
+                            <i class="far fa-envelope footer-icon"></i>
+                        </div>
+                    </td>
+                    <td style="padding-left: 10px">buituhoang@gmail.com</td>
+                </tr>
+                <tr>
+                    <td>
+                        <div class="crl-icon">
+                            <i class="fas fa-phone-alt footer-icon"></i>
+                        </div>
+                    </td>
+                    <td style="padding-left: 10px">081xxxx172</td>
+                </tr>
+                <tr>
+                    <td>
+                        <div class="crl-icon">
+                            <i class="fas fa-map-marker-alt footer-icon"></i></i>
+                        </div>
+                    </td>
+                    <td style="padding-left: 10px">Hanoi, Vietnam</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+    <div class="footer" align="center">
+        Copyright &copy;2021 All rights reserved
+    </div>
+        <br/>
+</footer>
---
+++ views/layouts/header.php
@@ -0,0 +1,26 @@
+<header>
+    <div class="topnav">
+        <a class="home" href="index.php?controller=posts">Home</a>
+        <?php
+        if(isset($_SESSION['session_user_id'])) {
+            echo '
+                <a class="logout" href="index.php?controller=auth&action=logout">Logout</a>
+                <a class="profile" href="index.php">Profile</a>
+            ';
+        }
+        else {
+            echo '
+                <a class="login" href="index.php?controller=auth&action=login">Login</a>
+                <a class="register" href="index.php?controller=auth&action=register"">Register</a>
+            ';
+        }
+        ?>
+
+        <div class="search-container">
+            <form action="/action_page.php">
+                <input type="text" placeholder="Search..." name="search">
+                <button class="fa fa-search" type="submit"></i></button>
+            </form>
+        </div>
+    </div>
+</header>
