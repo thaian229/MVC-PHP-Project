3b1a33bfe049be4f11b22f32139773a8fc639a2b
Minh Hoàng <52208309+mHoang99@users.noreply.github.com>
2021-06-04 15:16:58 +0700

    update user profile

--- config.php
+++ config.php
@@ -7,6 +7,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
 
+define('AVA_DIR', 'uploads/avatars/');
 
 define('BASE_PATH', 'http://localhost/NewPhpProject/MVC-PHP-Project/');
 
