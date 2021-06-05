90ff5b6edceea35fc9d54da52b133eaa2ca264bd
Minh Hoàng <52208309+mHoang99@users.noreply.github.com>
2021-06-06 01:23:47 +0700

    update

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 /** Configuration Variables **/
 
 define('DB_NAME', 'phim_online');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
+define('DB_USER', 'mhoang');
+define('DB_PASSWORD', '04126152');
 define('DB_HOST', 'localhost');
 
 define('AVA_DIR', 'uploads/avatars/');
