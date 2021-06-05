f8582a3b82084836202d91123297ddd23feebea8
Bùi Tú Hoàng <buituhoang@gmail.com>
2021-06-06 01:21:10 +0700

    Get vote

--- views/posts/show.php
+++ views/posts/show.php
@@ -1,5 +1,27 @@
 <script>
     const currentUrl = window.location.href
+
+    getVotedTypeVideo = () => {
+        var video_id = parseInt(currentUrl.split("&id=")[1])
+        let formData = new FormData();
+        formData.append('video_id', video_id);
+        console.log(video_id)
+        fetch('index.php?controller=posts&action=getVotedTypeVideo', {
+            method: "post",
+            body: formData,
+        })
+            .then(response => response.json())
+            .then(data => {
+                // var viewNumber = document.getElementById("view-number").value;
+                console.log(data)
+            })
+            .catch(e => {
+                console.log(e)
+            });
+    }
+
+    getVotedTypeVideo()
+
     sendComment = () => {
         var video_id = parseInt(currentUrl.split("&id=")[1])
         var content = document.getElementById('comment-input').value
@@ -41,9 +63,19 @@
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowfullscreen></iframe>
             <br/><br/>
-            <?php
-            echo $post->title;
-            ?>
+            <div>
+                <?php
+                echo $post->title;
+                ?>
+            </div>
+            <div>
+                <span>Views: </span>
+                <span id="view-number">
+                    <?php
+                    echo $post->views;
+                    ?>
+                </span>
+            </div>
         </div>
         <div class="show_column comments_part">
             <div id="comments">
@@ -54,10 +86,14 @@
                 }
                 ?>
             </div>
-            <form>
-                <input type="text" id="comment-input" placeholder="Write a comment"/>
-                <input type="button" id="comment-submit" value="Send Comment" onclick="sendComment()"/>
-            </form>
+            <?php
+                if(!empty($_SESSION['session_user_id'])) {
+                    echo '<form>';
+                    echo '<input type="text" id="comment-input" placeholder="Write a comment"/>';
+                    echo '<input type="button" id="comment-submit" value="Send Comment" onclick="sendComment()"/>';
+                    echo '</form>';
+                }
+            ?>
         </div>
     </div>
 </div>
--- views/user/index.php
+++ views/user/index.php
@@ -106,9 +106,10 @@
                     totalPage = data.body.totalPage
 
                     data.body.videos.forEach((video) => {
+                        console.log(video)
                         htmlString += `
                             <tr>
-                                <td width="250px"><img alt="NOT FOUND" width="200px" src=` + video.thumbnailUrl + `/></td>
+                                <td width="250px"><img alt="NOT FOUND" width="200px" src="` + video.thumbnailUrl + `"/></td>
                                 <td><a href="index.php?controller=posts&action=showPost&id=` + video.id + `">` + video.title + `</a></td>
                                 <td><button onclick="removeVideoFromFavouriteHandler()" id="remove-fav-`+ video.id +`">Remove</button></td>
                             </tr>
@@ -145,8 +146,8 @@
                         document.getElementById("back-to-first").setAttribute("disabled", "disabled");
                         document.getElementById("back-to-previous").setAttribute("disabled", "disabled");
                     } else {
-                        document.getElementById("go-to-last").removeAttribute("disabled")
-                        document.getElementById("go-to-next").removeAttribute("disabled")
+                        document.getElementById("back-to-first").removeAttribute("disabled")
+                        document.getElementById("back-to-previous").removeAttribute("disabled")
                     }
 
                 } else {
