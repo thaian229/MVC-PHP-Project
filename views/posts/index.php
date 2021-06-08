79e0ff776f060838c8cb0906640832d3733af620
Minh Hoàng <52208309+mHoang99@users.noreply.github.com>
2021-06-08 15:19:57 +0700

    update video card

--- views/posts/index.php
+++ views/posts/index.php
@@ -1,15 +1,17 @@
-<div class="container">
+<link rel="stylesheet" href="views/posts/index.css">
+
+<div class="container list-category-container">
     <?php
     for ($i = 0; $i < count($categories); $i++) {
-        echo '<div>';
+        echo '<div class=category-container>';
         echo '<div class="category-name"><h2>' . ucfirst($categories[$i]->catName) . '</h2></div>';
-        echo '<div>
+        echo '<div class="category-more">
                     <a href="index.php?controller=posts&action=getCategory&category=' . $categories[$i]->catName . '&page=1"
                     style="text-decoration: none;">
-                        View more...
+                        View more
                     </a>
               </div>';
-        echo '<div id="' . $categories[$i]->catName . '-videos"></div>';
+        echo '<div class="category-contents" id="' . $categories[$i]->catName . '-videos"></div>';
         echo '</div>';
     }
     ?>
@@ -23,9 +25,9 @@
         formData.append('category', categoryName);
         formData.append('page', 1);
         fetch('index.php?controller=posts&action=videosByCategory', {
-            method: "post",
-            body: formData,
-        })
+                method: "post",
+                body: formData,
+            })
             .then(response => response.json())
             .then(data => {
                 if (data.success == true) {
@@ -33,33 +35,34 @@
                     var videosByCategory = document.getElementById(data.body.category + '-videos')
                     data.body.videos.forEach((video) => {
                         videosByCategory.innerHTML += `
-                        <div>
-                            <a href="index.php?controller=posts&action=showPost&id=` + video.id + `" style="text-decoration: none;">
-                                <div class="thumbnail">
-                                    <img src="` + video.thumbnailUrl + `" width="240" height="180">
+                            <div id="video-` + video.id + `-card" class="video-card">
+                                <div class="video-card-overlay" id="video-` + video.id + `-card-overlay"></div>
+                                <img  class="video-card-thumbnail" src="` + video.thumbnailUrl + `"/>
+                                <div class="video-card-title">` + video.title + ` </div>
+                                <div class="video-card-info">
+                                    <div class="video-card-views">
+                                        <i class="fas fa-eye"></i>
+                                        <span>` + video.views + `</span>
+                                    </div>
+
+                                    <div class="video-card-liking">
+                                        <div class="video-card-like">
+                                            <i class="fas fa-thumbs-up"></i>
+                                            <div>` + video.upvotes + `</div>
+                                        </div>
+
+                                        <div class="video-card-dislike">
+                                            <i class="fas fa-thumbs-down"></i>
+                                            <div>` + video.downvotes + `</div>
+                                        </div>
+                                    </div>
                                 </div>
-                            </a>
-                        </div>
+                            </div>
                         <div>
-                            <a href="index.php?controller=posts&action=showPost&id=` + video.id + `" style="text-decoration: none;">
-                                <div class="title">
-                                    <p ><strong>` + video.title + `</strong></p>
-                                </div>
-                            </a>
-                        </div>
-                        <div>
-                            <div><i class="fas fa-eye"></i></div>
-                            <div><strong>` + video.views + `</strong></div>
-                        </div>
-                        <div>
-                            <div><i class="fas fa-thumbs-up"></i></div>
-                            <div><strong>` + video.upvotes + `</strong></div>
-                        </div>
-                        <div>
-                            <div><i class="fas fa-thumbs-down"></i></div>
-                            <div><strong>` + video.downvotes + `</strong></div>
-                        </div>
                         `
+
+                        let card = document.getElementById(`video-` + video.id + `-card-overlay`)
+                        card.addEventListener("click", onVideoClicked)
                     })
                 }
             })
@@ -68,4 +71,9 @@
             });
     }
 
+    onVideoClicked = (event) => {
+        console.log(event)
+        let vId = event.target.id.split("-")[1]
+        window.location.href = `index.php?controller=posts&action=showPost&id=` + vId
+    }
 </script>
